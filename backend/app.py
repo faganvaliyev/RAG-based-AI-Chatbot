ccc5fd04198119431f25081b514f883f1499ad0e
faganvaliyev <velievfeqan.04@gmail.com>
2025-08-22 21:18:43 +0400

    upt

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import StreamingResponse
 from src.api.v1 import routes
 
 app = FastAPI(title="RAG AI Chatbot Backend (LLM-only)")
